779a2554f16f4d98818e2c6d012266240d20fdf9
joshrhughes <josh@mtnpine.us>
2018-01-09 09:31:29 -0700

    working through sprint 2

--- public/js/app.js
+++ public/js/app.js
@@ -6,43 +6,41 @@
  */
 
 
-/* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-sampleAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-sampleAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-sampleAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
-/* end of hard-coded data */
-
-
-
-
 $(document).ready(function() {
   console.log('app.js loaded!');
+  //renderAlbum(sampleAlbums[0]);
+  //sampleAlbums.forEach(renderAlbum);
+
+  $.get('/api/albums', function(albums){
+    console.log('response returned');
+    console.log(albums);
+    albums.forEach(function(oneAlbum){
+      renderAlbum(oneAlbum);
+    });
+  });
+
+  //When Form Submits
+  $('#newForm').on('submit', function(event){
+    event.preventDefault();
+    var formData = $(this).serialize();
+    console.log("form data is " + $(this).serialize());
+    console.log("the unserialzied data is " + this);
+    console.log('formData', formData);
+    $(this).trigger("reset");
+  });
+
+  // $('#album-form form').on('submit', function (e) {
+  //   e.preventDefault();
+  //   var formData = $(this).serialize();
+  //   console.log('formData', formData);
+  //   $.post('/api/albums', formData, function (album) {
+  //     console.log('album after POST', album);
+  //     renderAlbum(album);  //render the server's response
+  //   });
+  //   $(this).trigger("reset");
+  // });
 
 });
-
-
 
 
 
@@ -52,7 +50,7 @@ function renderAlbum(album) {
 
   var albumHtml =
   "        <!-- one album -->" +
-  "        <div class='row album' data-album-id='" + "HARDCODED ALBUM ID" + "'>" +
+  "        <div class='row album' data-album-id='" + album._id + "'>" +
   "          <div class='col-md-10 col-md-offset-1'>" +
   "              <!-- begin album internal row -->" +
   "                <div class='row'>" +
@@ -63,15 +61,15 @@ function renderAlbum(album) {
   "                    <ul class='list-group'>" +
   "                      <li class='list-group-item'>" +
   "                        <h4 class='inline-header'>Album Name:</h4>" +
-  "                        <span class='album-name'>" + "HARDCODED ALBUM NAME" + "</span>" +
+  "                        <span class='album-name'>" + album.name + "</span>" +
   "                      </li>" +
   "                      <li class='list-group-item'>" +
   "                        <h4 class='inline-header'>Artist Name:</h4>" +
-  "                        <span class='artist-name'>" +  "HARDCODED ARTIST NAME"+ "</span>" +
+  "                        <span class='artist-name'>" + album.artistName + "</span>" +
   "                      </li>" +
   "                      <li class='list-group-item'>" +
   "                        <h4 class='inline-header'>Released date:</h4>" +
-  "                        <span class='album-releaseDate'>" + "HARDCODED ALBUM RELEASE" + "</span>" +
+  "                        <span class='album-releaseDate'>" + album.releaseDate + "</span>" +
   "                      </li>" +
   "                    </ul>" +
   "                  </div>" +
@@ -81,5 +79,5 @@ function renderAlbum(album) {
   "          <!-- end one album -->";
 
   // render to the page with jQuery
-
+$('#albums').append(albumHtml);
 }
